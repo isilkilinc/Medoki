521e9e0b5602dad5e446903219b0dd22ec2ae743
Işıl Kılınç <isilkilinc@Isls-MacBook-Air.local>
2026-03-26 12:30:48 +0300

    Medoki is finally fixed and working

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -18,8 +17,6 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      // Bu satırı ekledik: Kodun @/components aradığında direkt features içine bakacak!
-      "@/components": path.resolve(__dirname, "./src/features/components"),
     },
     dedupe: [
       "react",
