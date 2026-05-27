588f104f8db471dc575d615386b27a6d77a7d017
Işıl Kılınç <isilkilinc222@gmail.com>
2026-05-27 20:48:59 +0300

    Update App.tsx

--- features/App.tsx
+++ features/App.tsx
@@ -1,3 +1,4 @@
+import UpdatePassword from "@/features/pages/UpdatePassword";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
