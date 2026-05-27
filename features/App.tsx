5bab62d805c107a6c2753aa4a8d5ba74747cd9c3
Işıl Kılınç <isilkilinc222@gmail.com>
2026-05-27 21:46:51 +0300

    Update App.tsx

--- features/App.tsx
+++ features/App.tsx
@@ -1,5 +1,5 @@
 import UpdatePassword from "./pages/UpdatePassword";
-import CommunityScreen from "./pages/CommunityScreen"; // YENİ EKLENDİ
+import CommunityScreen from "./components/CommunityScreen"; // YENİ EKLENDİ
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
